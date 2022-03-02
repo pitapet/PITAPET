a1ca208f554e41b02361a3b90a8273a05ed11960
juhannah <jamongju96@gmail.com>
2022-03-03 01:54:39 +0900

    220303_product_상품번호 랜덤 추출

--- 99_PitAPet/src/main/java/com/kh/pitapet/product/controller/ProductController.java
+++ 99_PitAPet/src/main/java/com/kh/pitapet/product/controller/ProductController.java
@@ -1,12 +1,19 @@
 package com.kh.pitapet.product.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.kh.pitapet.member.model.vo.Member;
 import com.kh.pitapet.product.model.service.ProductService;
 import com.kh.pitapet.product.model.vo.Product;
 
@@ -22,16 +29,48 @@ public class ProductController {
 	
 	@GetMapping("/product")
 	public ModelAndView list(
-			ModelAndView model,
-			@RequestParam(value = "no", defaultValue = "1") int no) {
-
-		Product product = service.findProductByNo(no);
+			ModelAndView model) {
 		
-		System.out.println(product);
+		Product product = null;
+		List<Integer> pNo = new ArrayList<Integer>();
+		List<Integer> resultList = new ArrayList<Integer>();
+		
+		pNo = service.getProductNo();
+		
+		for(int i : pNo) {
+			if(!resultList.contains(i)) {
+				resultList.add(i);
+			}
+		}
+		
+		Collections.shuffle(resultList);
+		
+		int no = resultList.get(0);
+		
+		product = service.findProductByNo(no);
 		
 		model.addObject("product", product);
 		
 		model.setViewName("product/product");
+		
+		return model;
+	}
+	
+	@GetMapping("/add")
+	public String addProduct() {
+		
+		return "product/addProduct";
+	}
+	
+	@GetMapping("/productCheck")
+	public ModelAndView productCheck(
+			ModelAndView model,
+			@ModelAttribute Product product,
+			@SessionAttribute Member loginMember,
+			@SessionAttribute Product selectProduct) {
+		
+		
+		
 		
 		return model;
 	}
