f17b589effd47e678bc01b0f154df25772887663
jungjinl <limjj0128@naver.com>
2022-03-03 15:26:55 +0900

    220303_커뮤니티 리스트 구현중

---
+++ 99_PitAPet/src/main/java/com/kh/pitapet/community/model/dao/BoardMapper.java
@@ -0,0 +1,26 @@
+package com.kh.pitapet.community.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+import com.kh.pitapet.community.model.vo.Board;
+
+@Mapper
+public interface BoardMapper {
+	
+	int getBoardCount();
+
+	List<Board> findAll(RowBounds rowBounds);
+
+	Board selectBoardByNo(@Param("no") int no);
+	
+	int insertBoard(Board board);
+
+	int updateBoard(Board board);
+
+	int updateStatus(@Param("no") int no, @Param("status") String status);
+
+}
---
+++ 99_PitAPet/src/main/java/com/kh/pitapet/community/model/vo/Board.java
@@ -0,0 +1,39 @@
+package com.kh.pitapet.community.model.vo;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board {
+	private int no;
+	
+	private int rowNum;
+	
+	private int writerNo;
+	
+	private String writerId;
+	
+	private String title;
+	
+	private String content;
+	
+	private String originalFileName;
+	
+	private String renamedFileName;
+	
+	private int readCount;
+	
+	private String status;
+	
+	private List<Reply> replies;
+	
+	private Date createDate;
+	
+	private Date modifyDate;
+}
